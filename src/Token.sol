8f264c9ab21e85b64315f437d81320434e9aa0b3
Chase Brown <Chasebrown7@icloud.com>
2023-01-18 10:14:29 -0700

    removed duplicates

--- src/Token.sol
+++ src/Token.sol
@@ -254,10 +254,8 @@ contract Inuvation is ERC20, Ownable {
         utility4Address = utility4Addr;
 
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
-        excludeFromMaxTransaction(address(_uniswapV2Router), true);
         uniswapV2Router = _uniswapV2Router;
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory()).createPair(address(this), _uniswapV2Router.WETH());
-        excludeFromMaxTransaction(address(uniswapV2Pair), true);
         _setAutomatedMarketMakerPair(address(uniswapV2Pair), true);
 
         uint256 totalSupply = 100_000_000 * 10**_decimals;
